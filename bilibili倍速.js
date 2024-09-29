e3b4611094a308029bd2c085a1f36efb9085ee65
Yang Yuchen <67354273+dfdy-yyc@users.noreply.github.com>
2024-09-29 13:34:21 +0800

    Update and rename 测试.js to bilibili倍速.js

--- bilibili倍速.js
+++ bilibili倍速.js
@@ -1,12 +1,19 @@
 // ==UserScript==
-// @name         bilibil 倍速
-
-// @version      0.3
+// @name         bilibili 倍速
+// @license MIT
+// @version      0.4.1
 // @description  按钮倍速
 // @author       dfdy-yyc
-// @match        https://www.bilibili.com/video/*
+// @match           https://www.bilibili.com/video/*
+// @match           https://www.bilibili.com/list/*
+// @match           https://www.bilibili.com/bangumi/play/*
+// @match           https://www.bilibili.com/cheese/play/*
+// @match           https://www.bilibili.com/festival/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
+// @namespace https://greasyfork.org/users/1276921
+// @downloadURL https://update.greasyfork.org/scripts/490330/bilibili%20%E5%80%8D%E9%80%9F.user.js
+// @updateURL https://update.greasyfork.org/scripts/490330/bilibili%20%E5%80%8D%E9%80%9F.meta.js
 // ==/UserScript==
 
 (function() {
@@ -23,6 +30,7 @@ var intervalId = setInterval(function() {
         // 在这里编写需要执行的代码
          var parentElement1 = document.getElementsByClassName('bpx-player-ctrl-playbackrate-menu')[0]
         parentElement1.insertAdjacentHTML('afterbegin', '<li class="bpx-player-ctrl-playbackrate-menu-item" data-value="3.0">3.0x</li>');
+        parentElement1.insertAdjacentHTML('afterbegin', '<li class="bpx-player-ctrl-playbackrate-menu-item" data-value="4.0">4.0x</li>');
     }
 
 }, 100); // 每隔100毫秒执行一次搜索
